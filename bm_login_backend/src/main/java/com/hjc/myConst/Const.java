bf9bbd0db544b357b958ddce3559313e3bf2cc21
一只会爬的小呆瓜 <2020885569@qq.com>
2024-04-24 22:42:15 +0800

    后端：限流操作

--- bm_login_backend/src/main/java/com/hjc/myConst/Const.java
+++ bm_login_backend/src/main/java/com/hjc/myConst/Const.java
@@ -3,7 +3,11 @@ package com.hjc.myConst;
 public class Const {
     public static final String JWT_BLACK_LIST = "jwt:blacklist:";
 
-    public static final int ORDER_CORS = -102;
+    public static final int ORDER_CORS = -101;
+    public static final int ORDER_LIMIT = -102;
+
+    public static final String FLOW_LIMIT_COUNTER = "flow:counter:";
+    public static final String FLOW_LIMIT_BLOCK = "flow:block:";
 
     public static final String VERIFY_EMAIL_LIMIT = "verify:email:limit";
 
